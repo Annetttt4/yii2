2b4977d41f896d56515f4abcad4950eb3772f821
Annetttt4 <33448848+Annetttt4@users.noreply.github.com>
2019-05-16 18:17:46 +0300

    Update db.php

--- crm/config/db.php
+++ crm/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=crm',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=rksi-anna_db',
+    'username' => 'rksi-anna_user',
+    'password' => 'Lw6pmKcDJS',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
